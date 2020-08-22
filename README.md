aa29f271a3357fc7cfb57dac593259d8c20a08f4
dyunis <dyunis@ttic.edu>
2020-08-22 13:45:44 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## TODO:
+- html page layout
+- javascript for uploading wav files/recording from the mic (cap at 15secs)
+- python backend:
+  - pytorch code loading in wav2vec2 model and showing predictions
+  - soundfile for doing spectrogram transform on the fly and using as input to
+    RNN model
+- flask server
+- deploying with nginx?
--- templates/script.js
+++ templates/script.js
@@ -1,2 +1,28 @@
-let context = new AudioContext()
-let buffer = context.createBuffer(1, 160000, 16000)
+let context = new AudioContext({sampleRate: 16000})
+
+console.log('audio starting')
+
+let sample_rate = 16000
+let seconds = 10
+let samples = seconds * sample_rate
+
+let input = null
+let microphone_stream = null
+
+// prompt for mic permissions happens at navigator.getUserMedia
+if (!navigator.getUserMedia) {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+}
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({audio: true, video: false},
+    function (stream) {
+      start_mic(stream)
+    },
+    function (e) {
+      alert('error with audio capture')
+    }
+  )
+} else {
+  alert('getUserMedia not support in this browser')
+}
