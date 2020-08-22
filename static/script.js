6fc4b3aec61da29ba008cc6ff4b66578e7b9ae3f
dyunis <dyunis@ttic.edu>
2020-08-22 15:12:30 -0500

    simplifying js

--- static/script.js
+++ static/script.js
@@ -1,28 +1,13 @@
-let context = new AudioContext({sampleRate: 16000})
-
-console.log('audio starting')
-
-let sample_rate = 16000
-let seconds = 10
-let samples = seconds * sample_rate
-
-let input = null
-let microphone_stream = null
-
-// prompt for mic permissions happens at navigator.getUserMedia
-if (!navigator.getUserMedia) {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+function setupAudio () {
+  navigator.mediaDevices.getUserMedia({audio: true, video: false})
+    .then(startMic)
+    .catch(audioError)
 }
 
-if (navigator.getUserMedia) {
-  navigator.getUserMedia({audio: true, video: false},
-    function (stream) {
-      start_mic(stream)
-    },
-    function (e) {
-      alert('error with audio capture')
-    }
-  )
-} else {
-  alert('getUserMedia not support in this browser')
+function startMic (stream) {
+  let context = new AudioContext({sampleRate: 16000})
+}
+
+function audioError (error) {
+  alert('Audio capture error: ', error.code)
 }
