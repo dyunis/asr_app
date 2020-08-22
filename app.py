2c2a57755261242bc266f43ff64fee1c46e64690
dyunis <dyunis@ttic.edu>
2020-08-22 11:49:58 -0500

    html

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-import flask
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
 
 def main():
-    app = Flask(__name__)
     app.run(debug=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def input():
-    if flask.request.method == 'POST':
-        if 'file' not in flask.request.files:
-            return redirect(flask.request.url)
-        f = flask.request.files.get('file')
-        if not f:
-            return
-        # (read in wav file)
-        wav = 0
-        return flask.render_template('result.html', name=, description=)
+@app.route('/')
+def index():
+    return f'{[1, 3, 4]} this is the index'
 
-    return flask.render_template('index.html')
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World'
+
+# the path in app.route generates the html file path for the webapp
+
+# @app.route('/', methods=['GET', 'POST'])
+# def input():
+    # if request.method == 'POST':
+        # if 'file' not in request.files:
+            # return redirect(request.url)
+        # f = request.files.get('file')
+        # if not f:
+            # return
+        # # (read in wav file)
+        # wav = 0
+        # return render_template('result.html', name=, description=)
+# 
+    # return render_template('index.html')
 
 if __name__=='__main__':
     main()
