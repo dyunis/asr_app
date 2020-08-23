62cdd33348e34e9e559f9eede9130f6305052965
dyunis <dyunis@ttic.edu>
2020-08-23 17:15:03 -0500

    media recording

--- static/script.js
+++ static/script.js
@@ -1,13 +1,36 @@
 function setupAudio () {
-  navigator.mediaDevices.getUserMedia({audio: true, video: false})
+  let constraints = {audio: {channelCount: 1, sampleRate: 16000}}
+  navigator.mediaDevices.getUserMedia(constraints)
     .then(startMic)
     .catch(audioError)
 }
 
+// get max 15 seconds of data
+// map stream to blob with wav format
+// show in window
+// save blob to a wav file to use as input in backend
+// show backend result
+
 function startMic (stream) {
-  let context = new AudioContext({sampleRate: 16000})
+  const mediaRecorder = new MediaRecorder(stream)
+  mediaRecorder.start(160000)
+  console.log('starting media recorder')
+
+  let chunks = []
+  mediaRecorder.ondataavailable = function (e) {
+    chunks.push(e.data)
+    mediaRecorder.stop()
+  }
+
+  mediaRecorder.onstop = function(e) {
+    const blob = new Blob(chunks, {'type': 'audio/wav'})
+    chunks = []
+    const audioURL = window.URL.createObjectURL(blob)
+    // stop the stream at the end (revoke permissions)
+    stream.getAudioTracks()[0].stop()
+  }
 }
 
 function audioError (error) {
-  alert('Audio capture error: ', error.code)
+  alert('Audio capture error, reload the page if you already denied permissions: ', error.code)
 }
